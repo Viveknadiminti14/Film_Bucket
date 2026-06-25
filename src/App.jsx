2fc83732d3ab1c31b2e3f0fbcf1fa6549ab6d9a2
Vivek Chary <vivekchary80@gmail.com>
2026-06-25 12:53:18 +0530

    Fix Watchlist import case

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
-          <Route path="/WatchList" element={<WatchList />} />
+          <Route path="/Watchlist" element={<Watchlist />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/about" element={<About />} />
